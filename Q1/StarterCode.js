c3fe5bdda20be81a4342df824cc7a993d2acc4c7
Cheng Geng <No public email provided>
2021-06-07 12:40:05 +0800

    Changes made from Source Academy

--- Q1/StarterCode.js
+++ Q1/StarterCode.js
@@ -1,2 +1,12 @@
-// Your code here!
-kdfjglkdfjgkdfjg
+MCQ
+{
+  "questions":
+  [
+    { "solution": "Θ(1)", "hint":"" },
+    { "solution": "Θ(log _n_)", "hint":"" },
+    { "solution": "Θ(_n_)", "hint":"" },
+    { "solution": "Θ(_n_ log _n_)", "hint":"" },
+    { "solution": "Θ(_n_²)", "hint":"" }
+  ],
+  "answer": -1
+}
